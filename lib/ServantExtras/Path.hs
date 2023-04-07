932fa7b475bdf7089ed6e1821db02ad345b27d5f
Adam McCullough <amccullough@fpcomplete.com>
2023-04-06 22:53:27 -0700

    Add more haddock goodness.

--- lib/ServantExtras/Path.hs
+++ lib/ServantExtras/Path.hs
@@ -1,9 +1,36 @@
+-- |Description: Provides a combinator to give handlers access to the
+-- PathInfo field from the WAI request.
 module ServantExtras.Path where
 
 import Data.Text (Text)
 import Network.Wai
 import Servant
 import Servant.Server.Internal.Delayed (passToServer)
+
+{-|
+  @PathInfo@ provides the `Network.Wai.pathInfo` field from the WAI request.
+
+  Example:
+
+@
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text)
+import Servant
+import ServantExtras.Path
+
+type MyAPI = "my-path-endpoint"
+           :> PathInfo
+           :> Get '[JSON] NoContent
+
+myServer :: Server MyAPI
+myServer = pathEndpointHandler
+  where
+    pathEndpointHandler :: [Text] -> Handler NoContent
+    pathEndpointHandler pInfo =
+      -- Do something clever with the path fragments.
+      pure NoContent
+@
+-}
 
 data PathInfo
 
--- lib/ServantExtras/RawRequest.hs
+++ lib/ServantExtras/RawRequest.hs
@@ -1,9 +1,35 @@
+-- |Description: Provide a combinator to give handlers access to the
+-- raw WAI request.
 module ServantExtras.RawRequest where
 
 import Network.Wai
 import Servant
 import Servant.Server.Internal.Delayed (passToServer)
 
+{-|
+  @RawRequest@ provides the `Network.Wai.Request` field from the WAI request.
+
+  Example:
+
+@
+import Control.Monad.IO.Class (liftIO)
+import Network.Wai
+import Servant
+import ServantExtras.RawRequest
+
+type MyAPI = "my-request-endpoint"
+           :> RawRequest
+           :> Get '[JSON] NoContent
+
+myServer :: Server MyAPI
+myServer = requestEndpointHandler
+  where
+    requestEndpointHandler :: Request -> Handler NoContent
+    requestEndpointHandler req =
+      -- Do something clever with the request
+      pure NoContent
+@
+-}
 data RawRequest
 
 instance HasServer api ctx => HasServer (RawRequest :> api) ctx where
